fd3525aa4c69bb1cb5cef157dcf6d776daed7996
laptop <juha.jylhakoski@aalto.fi>
2021-06-30 18:13:32 +0800

    3.5 3.6

--- index.js
+++ index.js
@@ -26,15 +26,113 @@ let persons = [
     }
   ]
 
+// 3.5
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// 3.5
+const generateId = () => {
+  const max = 8589934592
+  const min = persons.length
+  //const min = persons.length > 0
+  //  ? Math.max(...persons.map(n => n.id))
+  //  : 0console.log('id', id)
+  console.log('min', min)
+
+  id = getRandomInt(min,max)
+
+  console.log('id', id)
+
+  return id
+}
+
+// 3.6
+function isObjectEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+// 3.6
+function isKeyEmpty(key) {
+  return (key === undefined || key == null || key.length <= 0) ? true : false;
+}
+
+function hasKeyValue(obj, value) {
+  console.log('obj', obj)
+  console.log('value', value)
+  
+  for (var i=0; i < obj.length; i++) {
+    console.log('obj[i].name]', obj[i].name)
+    if(obj[i].name == value) {
+      console.log('return', true)
+      return true
+    }
+  }
+  console.log('return', false)
+  return false
+}
+
+function checkValidity(request, response) {
+
+  const content = request.body
+
+  if(isObjectEmpty(content)) {
+    console.log('error: content is empty')
+    return response.status(400).json({
+    status: 'error',
+    error: 'error: request content is empty',
+  }) }
+  /* else if (content.hasOwnProperty('name')) {
+    console.log('error: has not name')
+    return response.status(400).json({error: 'error: has not name'})
+  }*/
+  else if (isKeyEmpty(content.name)) {
+    console.log('error: name is empty')
+    return response.status(400).json({
+      status: 'error',
+      error: 'error: name is empty'})
+  }
+
+  /*else if (content.hasOwnProperty('phone')) {
+    console.log('error: has not phone')
+    return response.status(400).json({error: 'error: has not phone'})
+  }*/
+  else if (isKeyEmpty(content.phone)) {
+    console.log('error: phone is empty')
+    return response.status(400).json({
+      status: 'error',
+      error: 'error: phone is empty'})
+  }
+  else {
+    console.log('content.name', content.name)
+
+    const exists = hasKeyValue(persons,content.name)
+
+    console.log('exists', exists)
+
+    if (exists) {
+      console.log('error: name must be unique')
+      return response.status(400).json({ 
+        status: 'error',
+        error: 'error: name must be unique'})
+    }
+  }
+
+  return null
+}
+
+// 3.0
 app.get('/', (request, response) => {
   response.send('<h1></h1>')
 })
 
+// 3.1
 app.get('/api/persons', (request, response) => {
   console.log(persons)
-  response.json(persons)
+  return response.json(persons)
 })
 
+// 3.2
 app.get('/info', (request, response) => {
 
   let dateTime = new Date();
@@ -45,12 +143,13 @@ app.get('/info', (request, response) => {
 
   console.log(info)
 
-  response.send(info)
+  return response.send(info)
 
 })
 
 // $ curl -X "GET" "http://localhost:3001/api/persons/2"
 
+// 3.3
 app.get('/api/persons/:id', (request, response) => {
 
   const id = Number(request.params.id)
@@ -70,11 +169,11 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end()
 
   }
-
 })
 
 // $ curl -X "DELETE" "http://localhost:3001/api/persons/2"
 
+// 3.4
 app.delete('/api/persons/:id', (request, response) => {
 
   const id = Number(request.params.id)
@@ -98,10 +197,40 @@ app.delete('/api/persons/:id', (request, response) => {
   } else {
 
     response.status(404).end()
-    
+
   }
 })
 
+// 3.5, 3.6
+app.post('/api/persons', (request, response) => {
+
+  console.log('request.body', request.body)
+
+  const content = request.body
+
+  var result = null
+
+  result = checkValidity(request,response)
+
+  console.log('result', result)
+
+  if (result == null) {
+
+    const person = {
+      name: content.name,
+      phone: content.phone,
+      id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    console.log(person)
+
+    return response.json(person)
+  }
+})
+
+//curl -X POST http://localhost:3001/api/persons -H "Content-Type: application/json" -d "{\"name\":\"Jane Austin\", \"phone\":\"0123456789\"}"
 
 const PORT = 3001
 
